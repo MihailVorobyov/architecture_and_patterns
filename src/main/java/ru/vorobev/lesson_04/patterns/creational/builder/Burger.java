47b83de36e65c3165a69085083edc119ed797a03
Mihail_Vorobyov <ptichkinm@mail.ru>
2021-12-16 22:27:55 +0300

    Builder

---
+++ src/main/java/ru/vorobev/lesson_04/patterns/creational/builder/Burger.java
@@ -0,0 +1,20 @@
+package ru.vorobev.lesson_04.patterns.creational.builder;
+
+public class Burger {
+	
+	private final int size;
+	
+	private final boolean cheese;
+	private final boolean pepperoni;
+	private final boolean lettuce;
+	private final boolean tomato;
+	
+	public Burger(BurgerBuilder builder)
+	{
+		this.size = builder.size;
+		this.cheese = builder.cheese;
+		this.pepperoni = builder.pepperoni;
+		this.lettuce = builder.lettuce;
+		this.tomato = builder.tomato;
+	}
+}
