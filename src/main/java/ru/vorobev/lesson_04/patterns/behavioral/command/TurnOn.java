92914499d30fe908287c63d5d2462ab60284c0ed
Mihail_Vorobyov <ptichkinm@mail.ru>
2021-12-17 22:49:56 +0300

    Command

---
+++ src/main/java/ru/vorobev/lesson_04/patterns/behavioral/command/TurnOn.java
@@ -0,0 +1,25 @@
+package ru.vorobev.lesson_04.patterns.behavioral.command;
+
+public class TurnOn implements Command {
+	
+	private Bulb bulb;
+	
+	public TurnOn(Bulb bulb) {
+		this.bulb = bulb;
+	}
+	
+	@Override
+	public void execute() {
+		bulb.turnOn();
+	}
+	
+	@Override
+	public void undo() {
+		bulb.turnOff();
+	}
+	
+	@Override
+	public void redo() {
+		execute();
+	}
+}
