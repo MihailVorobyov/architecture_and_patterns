eb2fac8af32d2347499afba6974a0f7670cca1af
Mihail_Vorobyov <ptichkinm@mail.ru>
2021-12-25 23:30:57 +0300

    Реализовано обращение к БД

---
+++ src/main/java/ru/vorobev/lesson_04/patterns/system_patterns/orm/User.java
@@ -0,0 +1,35 @@
+package ru.vorobev.lesson_04.patterns.system_patterns.orm;
+
+public class User {
+
+	private long id;
+	private String login;
+	private String password;
+	
+	public User(long id, String login, String password) {
+		this.id = id;
+		this.login = login;
+		this.password = password;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	@Override
+	public String toString() {
+		return "User{" +
+			"id=" + id +
+			", login='" + login + '\'' +
+			", password='" + password + '\'' +
+			'}';
+	}
+}
